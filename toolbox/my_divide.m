1bd16f925ae02b3009611e6294934d1211cb1327
apuvvala <apuvvala@VDI-WD1AH2-408>
2025-09-15 10:53:02 -0400

    Update divide

--- toolbox/my_divide.m
+++ toolbox/my_divide.m
@@ -1,5 +1,8 @@
 function out = my_divide(in1,in2)
-
+arguments
+    in1 {mustBeNumeric}
+    in2 {mustBeNumeric}
+end
 if in2 == 0
     error("MATLAB:DivideByZero", "Division by zero is not allowed.");
 end
